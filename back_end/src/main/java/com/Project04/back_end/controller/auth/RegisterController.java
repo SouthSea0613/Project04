fcb51e1c492ff88a1c42855c7f25e25828885d90
South_Sea <tksz0613@gmail.com>
2025-06-19 16:40:18 +0900

    update

--- back_end/src/main/java/com/Project04/back_end/controller/auth/RegisterController.java
+++ back_end/src/main/java/com/Project04/back_end/controller/auth/RegisterController.java
@@ -1,15 +1,24 @@
 package com.Project04.back_end.controller.auth;
 
+import com.Project04.back_end.dto.UserDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
-@Controller
+@Controller("/auth")
 public class RegisterController {
     @GetMapping("/register")
     public String register() {
         log.info("register()");
         return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserDto userDto) {
+        // 뭘 해야하지
+        return "auth/login";
     }
 }
